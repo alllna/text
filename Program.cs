a1712527c28804fc906f944a7caf9a24b1a1e83f
alllna <alinazbaeva@gmail.com>
2025-02-14 15:13:29 +0300

    Файлы программы #c

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+
+public class Rectangle
+{
+    private double width;
+    private double height;
+
+    public Rectangle()
+    {
+        width = 1.0;
+        height = 1.0;
+    }
+
+    public Rectangle(double w, double h)
+    {
+        Width = w;
+        Height = h;
+    }
+
+    public double Width
+    {
+        get { return width; }
+        set
+        {
+            if (value > 0)
+                width = value;
+            else
+                throw new ArgumentException("Ширина должна быть положительной.");
+        }
+    }
+
+    public double Height
+    {
+        get { return height; }
+        set
+        {
+            if (value > 0)
+                height = value;
+            else
+                throw new ArgumentException("Высота должна быть положительной.");
+        }
+    }
+
+    public double Area()
+    {
+        return width * height;
+    }
+
+    ~Rectangle()
+    {
+        Console.WriteLine("Объект Rectangle уничтожен.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Rectangle rect = new Rectangle(5.0, 3.0);
+        Console.WriteLine($"Площадь прямоугольника: {rect.Area()}");
+    }
+}
+
+
